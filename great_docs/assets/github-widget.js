397362def6bde647d41dee70b0883a98ada67919
Richard Iannone <riannone@me.com>
2026-03-13 12:50:24 -0400

    Increase dropdown SVG arrow size to 16px

--- great_docs/assets/github-widget.js
+++ great_docs/assets/github-widget.js
@@ -68,7 +68,7 @@
                         <span class="gh-count" data-stat="forks">-</span>
                     </span>
                 </span>
-                <svg class="gh-dropdown-arrow" viewBox="0 0 16 16" width="12" height="12" fill="currentColor" aria-hidden="true">
+                <svg class="gh-dropdown-arrow" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" aria-hidden="true">
                     <path d="M4.427 7.427l3.396 3.396a.25.25 0 00.354 0l3.396-3.396A.25.25 0 0011.396 7H4.604a.25.25 0 00-.177.427z"/>
                 </svg>
             </div>
