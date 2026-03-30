4a4e7857ee3f1442a190dc7452d239f7fea07b30
Richard Iannone <riannone@me.com>
2026-03-30 15:15:56 -0400

    Remove redundant license_id fallback

--- great_docs/core.py
+++ great_docs/core.py
@@ -7480,16 +7480,6 @@ title: "Security Policy"
                 community_items.append(f"{spdx_html}<br>")
             else:
                 community_items.append(f"[{_full_license}]({license_link})<br>")
-        elif license_id:
-            _full_license = get_translation("full_license", lang)
-            if license_info is not None:
-                community_items.append(
-                    f"{_full_license} "
-                    f'<span title="{license_info.full_name}" '
-                    f'class="gd-spdx-badge">{license_info.spdx_id}</span><br>'
-                )
-            else:
-                community_items.append(f"{_full_license}: {license_id}<br>")
 
         # Citation (folded into Community)
         if citation_link:
