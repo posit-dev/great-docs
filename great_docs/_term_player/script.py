dfa2e10ec3b9318910872f4689d087353f884cff
Richard Iannone <riannone@me.com>
2026-05-22 16:48:42 -0400

    Add Cut and Annotation dataclasses

--- great_docs/_term_player/script.py
+++ great_docs/_term_player/script.py
@@ -17,3 +17,25 @@ class Chapter:
     label: str
 
 
+@dataclass
+class Cut:
+    """A segment to remove from playback."""
+
+    start: float
+    end: float
+    type: str = "jump"  # "jump", "ellipsis"
+
+
+@dataclass
+class Annotation:
+    """A floating annotation overlay."""
+
+    time: float
+    duration: float
+    text: str
+    position: str = "bottom-right"
+    style: str = "callout"
+    row: int | None = None
+    col: int | None = None
+
+
