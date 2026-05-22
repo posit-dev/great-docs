7c28f5222d733ee6e1f742385db6f87a618a258f
Richard Iannone <riannone@me.com>
2026-05-22 16:47:07 -0400

    Add Manifest.to_json serializer

--- great_docs/_term_player/manifest.py
+++ great_docs/_term_player/manifest.py
@@ -56,3 +56,46 @@ class Manifest:
     annotations: list[Annotation] = field(default_factory=list)
     highlights: list[Highlight] = field(default_factory=list)
 
+    def to_json(self) -> str:
+        """Serialize manifest to JSON string."""
+        data = {
+            "version": self.version,
+            "title": self.title,
+            "duration": round(self.duration, 3),
+            "term": {"cols": self.cols, "rows": self.rows},
+            "theme": self.theme,
+            "chapters": [{"time": round(ch.time, 3), "label": ch.label} for ch in self.chapters],
+            "keyframes": [{"time": round(kf.time, 3), "file": kf.file} for kf in self.keyframes],
+            "deltas": [
+                {
+                    "time": round(d.time, 3),
+                    "changes": [_delta_change_to_dict(c) for c in d.changes],
+                }
+                for d in self.deltas
+            ],
+            "annotations": [
+                {
+                    "time": round(a.time, 3),
+                    "duration": round(a.duration, 3),
+                    "text": a.text,
+                    "position": a.position,
+                    "style": a.style,
+                }
+                for a in self.annotations
+            ],
+            "highlights": [
+                {
+                    "time": round(h.time, 3),
+                    "duration": round(h.duration, 3),
+                    "row": h.row,
+                    "col": h.col,
+                    "width": h.width,
+                    "height": h.height,
+                    "style": h.style,
+                }
+                for h in self.highlights
+            ],
+        }
+        return json.dumps(data, indent=2)
+
+
